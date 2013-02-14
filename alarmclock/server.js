1053569249d12d4ba8e9210725b3c98dfe49d0d5
R. S. Doiel <rsdoiel@gmail.com>
2013-02-13 21:42:34 -0800

    Started playing wiht YQL for weather/location data

--- alarmclock/server.js
+++ alarmclock/server.js
@@ -4,16 +4,9 @@
 var express = require("express"),
 	server = express();
 
-server.get("/", function (request, response) {
-	response.sendfile("./index.html");
-});
-
-server.get("/style.css", function (request, response) {
-	response.sendfile("./style.css");
-});
-
-server.get("/alarmclock.js", function (request, response) {
-	response.sendfile("./alarmclock.js");
-});
+server.use(express.logger());
+console.log("Serving files from", __dirname);
+server.use(express.static(__dirname));
 
 server.listen(process.env.PORT || 3000);
+console.log("Listening on port", process.env.PORT || 3000);
