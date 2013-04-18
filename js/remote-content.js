87604beeb6ff858c002dabe163436791ce21ddf1
R. S. Doiel <rsdoiel@gmail.com>
2013-04-18 06:40:17 -0700

    Rewrote js/remote-content.js to use YUI3.

--- js/remote-content.js
+++ js/remote-content.js
@@ -2,121 +2,136 @@
  * Setup a layout for bringing in content from various places on 
  * the net.
  */
+/*jslint browser: true, undef: true, newcaps: true, indent: 2 */
+/*global YUI */
+YUI().use("node-base", "io", function (Y) {
+  "use strict";
+  /**
+   * Setup and layout content for activity at Google Reader
+   */
+  var makeReaderDeck = function (item) {
+    var title;
 
-/**
- * Setup and layout content for activity at Google Reader
- */
-makeReaderDeck = function (item) {
-  var title;
-  if (typeof item.alternate.href !== "undefined") {
-    title = '<a href="' + item.alternate.href + '">' + item.title + '</a>';
-  } else {
-    title = item.title;
-  }
-  if (typeof item.summary !== "undefined") {
-    return '<li>' + title + ' -<br />' + item.summary + '</li>';
-  }
-  return '<li>' + title + '</li>';
-};
+    if (typeof item.alternate.href !== "undefined") {
+      title = '<a href="' + item.alternate.href + '">' + item.title + '</a>';
+    } else {
+      title = item.title;
+    }
+    if (typeof item.summary !== "undefined") {
+      return '<li>' + title + ' -<br />' + item.summary + '</li>';
+    }
+    return '<li>' + title + '</li>';
+  };
 
-/**
- * Setup a layout for bringing in content from Blogger
- */
-makeBloggerDeck = function (item) {
-  var title, j = 0;
+  /**
+   * Setup a layout for bringing in content from Blogger
+   */
+  var makeBloggerDeck = function (item) {
+    var title, j = 0;
   
-  if (typeof item.link !== "undefined") {
-    j = 0;
-    while(j < item.link.length) {
-      if (item.link[j].rel === 'alternate') {
-        title = '<a href="' + item.link[j].href + '">' + item.title.$t + '</a>';
-        j = item.link.length;
+    if (typeof item.link !== "undefined") {
+      j = 0;
+      while(j < item.link.length) {
+        if (item.link[j].rel === 'alternate') {
+          title = '<a href="' + item.link[j].href + '">' + item.title.$t + '</a>';
+          j = item.link.length;
+        }
+        j += 1;
       }
-      j += 1;
+    } else {
+      title = item.title.$t;
     }
-  } else {
-    title = item.title.$t;
-  }
-  if (typeof item.summary !== "undefined") {
-    return '<li>' + title + ' -<br />' + item.summary.$t + '</li>';
-  }
-  return '<li>' + title + '</li>';
-};
+    if (typeof item.summary !== "undefined") {
+      return '<li>' + title + ' -<br />' + item.summary.$t + '</li>';
+    }
+    return '<li>' + title + '</li>';
+  };
 
-/**
- * Setup and layout content pulls for Github
- */
-makeGithubList = function (items) {
-  var i = 0, parts = [];
+  /**
+   * Setup and layout content pulls for Github
+   */
+  var makeGithubList = function (items) {
+    var i = 0, parts = [];
 
-//  parts.push('<ul>');
-  Object.keys(items).forEach(function (ky, i) {
-  	var name = items[ky].name,
-		description = items[ky].description,
-		url = items[ky].url,
-		language = items[ky].language;
-		if (! language) {
+    //  parts.push('<ul>');
+    Object.keys(items).forEach(function (ky, i) {
+      var name = items[ky].name,
+		  description = items[ky].description,
+		  url = items[ky].url,
+		  language = items[ky].language;
+		  if (! language) {
 			language = "text";
-		}
-  	parts.push('<li><a href="' +
-		url + '">' + name + '</a> (' + language + ') ' + description + '</li>');
-  });
- // parts.push('</ul>');
-  return parts.join("");
-};
+		  }
+  	  parts.push('<li><a href="' +
+		  url + '">' + name + '</a> (' + language + ') ' + description + '</li>');
+    });
+    // parts.push('</ul>');
+    return parts.join("");
+  };
 
 
-//https://github.com/rsdoiel.atom
-$(document).ready(function () {
-  /* Grab my shared items from Google Reader. */
-  var urls = { 'google_reader': 'http://www.google.com/reader/public/javascript/user/05065097874671388976/state/com.google/broadcast?n=17',
-    	'github_rsdoiel' : 'https://github.com/rsdoiel.json',
-    	'github_uscwebservices' : 'https://github.com/uscwebservices.json',
-    	'phizcode_blog' : 'http://phizcode.blogspot.com/feeds/posts/default?max-results=3&alt=json'
-     };
-  
-  $.getJSON(urls.google_reader + '&callback=?', function (data) {
-    for (var i = 0; i < data.items.length; i += 1) {
-      $('#recently-read').append(makeReaderDeck(data.items[i]));
-    }
-  });
+  var urls = { 
+    'github_rsdoiel' : 'https://github.com/rsdoiel.json',
+ 	'github_uscwebservices' : 'https://github.com/uscwebservices.json',
+   	'phizcode_blog' : 'http://phizcode.blogspot.com/feeds/posts/default?max-results=3&alt=json'
+  };
   
   /* urls.github_my_repos */
-  $.getJSON(urls.github_rsdoiel + '?callback=?', function (data) {  
-  	var i = 0, repos = {};
-    if (data.length > 0) {
-    	for (i = 0; i < data.length; i += 1) {
-		if (data[i].repository !== undefined &&
-				(data[i].repository.owner === "rsdoiel" ||
-				data[i].repository.owner === "uscwebservices")) {
-			if (repos[data[i].repository.name] === undefined) {
-				repos[data[i].repository.name] = data[i].repository;
-			}
-		}
-	}
-        $('#github-my-repos').append(makeGithubList(repos));
-    } else {
-        $('#github-my-repos').append("Can't reach github.com");
-    }
+  Y.io(urls.github_rsdoiel, {
+      on: {
+          success: function (id, response, args) {  
+              var data,
+  	            i = 0, repos = {};
+                try {
+                    data = JSON.parse(reponse.responseText);
+                } catch (e) {
+                    Y.one('#github-my-repos').append(e);
+                    return;
+                }
+                if (data.length > 0) {
+    	            for (i = 0; i < data.length; i += 1) {
+		                if (data[i].repository !== undefined &&
+				             (data[i].repository.owner === "rsdoiel" ||
+				            data[i].repository.owner === "uscwebservices")) {
+			                if (repos[data[i].repository.name] === undefined) {
+				                repos[data[i].repository.name] = data[i].repository;
+			                }
+		                }
+	                }
+                    Y.one('#github-my-repos').append(makeGithubList(repos));
+                } else {
+                    Y.one('#github-my-repos').append("Can't reach github.com");
+                }
+        }
+      }
   });
 
   /* urls.github_uscwebservices_repos */
-  $.getJSON(urls.github_uscwebservices + '?callback=?', function (data) {  
-  	var i = 0, repos = {};
-    if (data.length > 0) {
-    	for (i = 0; i < data.length; i += 1) {
-		if (data[i].repository !== undefined &&
-				(data[i].repository.owner === "rsdoiel" ||
-				data[i].repository.owner === "uscwebservices")) {
-			if (repos[data[i].repository.name] === undefined) {
-				repos[data[i].repository.name] = data[i].repository;
-			}
-		}
-	}
-        $('#github-uscwebservices-repos').append(makeGithubList(repos));
-    } else {
-        $('#github-uscwebservices-repos').append("Can't reach github.com");
-    }
+  Y.io(urls.github_uscwebservices, {
+      on: {
+          success: function (id, response, args) {  
+  	        var data, i = 0, repos = {};
+                try {
+                    data = JSON.parse(reponse.responseText);
+                } catch (e) {
+                    Y.one('#github-uscwebservices-repos').append(e);
+                    return;
+                }
+            if (data.length > 0) {
+    	        for (i = 0; i < data.length; i += 1) {
+		            if (data[i].repository !== undefined &&
+				            (data[i].repository.owner === "rsdoiel" ||
+				        data[i].repository.owner === "uscwebservices")) {
+			            if (repos[data[i].repository.name] === undefined) {
+				            repos[data[i].repository.name] = data[i].repository;
+			            }
+		            }
+	            }
+                Y.one('#github-uscwebservices-repos').append(makeGithubList(repos));
+            } else {
+                Y.one('#github-uscwebservices-repos').append("Can't reach github.com");
+            }
+         }
+      }
   });
 });
-
