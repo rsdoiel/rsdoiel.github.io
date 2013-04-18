8ab36a54a790ab4007dfabd9c4304f37be990dad
R. S. Doiel <rsdoiel@gmail.com>
2013-04-18 08:31:17 -0700

    Update remote-content.js

--- js/remote-content.js
+++ js/remote-content.js
@@ -2,69 +2,43 @@
  * Setup a layout for bringing in content from various places on 
  * the net.
  */
-/*jslint browser: true, undef: true, newcaps: true, indent: 2 */
+/*jslint browser: true, undef: true, newcaps: true, indent: 4 */
 /*global YUI */
 YUI().use("node-base", "jsonp", function (Y) {
-  "use strict";
+    "use strict";
 
-  /**
-   * Setup and layout content pulls for Github
-   */
-  var makeGithubList = function (items) {
-    var i = 0, parts = [];
+    /**
+     * Setup and layout content pulls for Github
+     */
+    function makeGithubList (items) {
+        var i = 0, parts = [];
+        
+        Y.log(items, "debug");
+        Object.keys(items).forEach(function (ky, i) {
+            var name = items[ky].name,
+                description = items[ky].description,
+                url = items[ky].url,
+                language = items[ky].language;
+            if (! language) {
+                language = "text";
+            }
+            parts.push('<li><a href="' +
+            url + '">' + name + '</a> (' + language + ') ' + description + '</li>');
+        });
+        return parts.join("");
+    }
 
-    Y.log(items, "debug");
-    //  parts.push('<ul>');
-    Object.keys(items).forEach(function (ky, i) {
-      var name = items[ky].name,
-		  description = items[ky].description,
-		  url = items[ky].url,
-		  language = items[ky].language;
-		  if (! language) {
-			language = "text";
-		  }
-          parts.push('<li><a href="' +
-		    url + '">' + name + '</a> (' + language + ') ' + description + '</li>');
+    /* urls.github_my_repos */
+    Y.jsonp("https://github.com/rsdoiel.json?callback={callback}", function (data) {  
+        var i = 0, repos = {};
+        Y.log(data, "debug");
+        if (data.length > 0) {
+            for (i = 0; i < data.length; i += 1) {
+                if (typeof data[i].repository !== "undefined")) {
+                    repos[data[i].repository.name] = data[i].repository;
+                }
+            }
+            Y.one('#github-repos').setHTML(makeGithubList(repos));
+        }
     });
-    // parts.push('</ul>');
-    return parts.join("");
-  };
-
-
-  var urls = { 
-    'github_rsdoiel' : 'https://github.com/rsdoiel.json',
- 	'github_uscwebservices' : 'https://github.com/uscwebservices.json',
-   	'phizcode_blog' : 'http://phizcode.blogspot.com/feeds/posts/default?max-results=3&alt=json'
-  };
-  
-  /* urls.github_my_repos */
-  Y.jsonp(urls.github_rsdoiel + "?callback={callback}", function (data) {  
-    var i = 0, repos = {};
-    Y.log(data, "debug");
-    if (data.length > 0) {
-      for (i = 0; i < data.length; i += 1) {
-        if (data[i].repository !== undefined &&
-            (data[i].repository.owner === "rsdoiel" ||
-              data[i].repository.owner === "uscwebservices")) {
-          repos[data[i].repository.name] = data[i].repository;
-        }
-      }
-      Y.one('#github-my-repos').setHTML(makeGithubList(repos));
-    }
-  });
-
-  /* urls.github_uscwebservices_repos */
-  Y.jsonp(urls.github_uscwebservices, function (data) {  
-    var i = 0, repos = {};
-    if (data.length > 0) {
-      for (i = 0; i < data.length; i += 1) {
-        if (data[i].repository !== undefined &&
-            (data[i].repository.owner === "rsdoiel" ||
-              data[i].repository.owner === "uscwebservices")) {
-          repos[data[i].repository.name] = data[i].repository;
-        }
-      }
-      Y.one('#github-uscwebservices-repos').setHTML(makeGithubList(repos));
-    }
-  });
 });
