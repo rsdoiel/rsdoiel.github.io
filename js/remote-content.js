8e53fb3ac342bf9de1a87c7aa5fdcb542c30575d
R. S. Doiel <rsdoiel@gmail.com>
2013-04-18 08:33:16 -0700

    Update remote-content.js

--- js/remote-content.js
+++ js/remote-content.js
@@ -34,7 +34,7 @@ YUI().use("node-base", "jsonp", function (Y) {
         Y.log(data, "debug");
         if (data.length > 0) {
             for (i = 0; i < data.length; i += 1) {
-                if (typeof data[i].repository !== "undefined")) {
+                if (typeof data[i].repository !== "undefined") {
                     repos[data[i].repository.name] = data[i].repository;
                 }
             }
