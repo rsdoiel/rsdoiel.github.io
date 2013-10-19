f37468cecb486a4a0d1c274ecdb85fcb939fab57
R. S. Doiel <rsdoiel@gmail.com>
2013-10-19 14:16:36 -0700

    snapshot

---
+++ ypg2yui3/steps-one-two.js
@@ -0,0 +1,17 @@
+YUI().use("node", function (Y) {
+    "use strict";
+    // Step 1, get a handle to the node you want to work with in 
+    // this case an anchor element
+    var anchor = Y.one("a"),
+        // Now get the value of the href attribute.
+        href = anchor.get("href"),
+        // Get the innerHTML of the anchor
+        innerHTML = anchor.get("innerHTML");
+
+    // Too see the results in the JavaScript console of the browser 
+    // use Y.log()
+    Y.log("The href is " + href);
+    Y.log("The innerHTML is " + innerHTML);
+    // Step 2, use Y.set() to change the value wrapped by the anchor
+    Y.setHTML('(links to: <em>' + href + '</em>) '  + innerHTML);
+});
