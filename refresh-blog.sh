3284219f28b1dbd0ce91f2353b0b5f91ccc5554d
R. S. Doiel <rsdoiel@gmail.com>
2016-05-31 08:25:49 -0700

    refreshed blog

--- refresh-blog.sh
+++ refresh-blog.sh
@@ -64,8 +64,7 @@ findfile -s .md ${POST_PATH:0:4} | sort -r | while read ITEM; do
     echo "Processing index.md <-- ${POST_PATH:0:4}/$ITEM"
     POST_FILENAME=${POST_PATH:0:4}/$ITEM
     POST_TITLE=$(fileTitle "$POST_FILENAME")
-    REL_PATH="${PATH_POST:0:4}/$ITEM"
-    echo "DEBUG relpath: $REL_PATH"
+    REL_PATH="${POST_PATH:0:4}/$ITEM"
     POST_DATE=$(pathparts -d $REL_PATH)
     POST_DATE=${POST_DATE//\//-}
     echo "+ [$POST_TITLE](/blog/${POST_PATH:0:4}/${ITEM/.md/.html}), $POST_DATE" >> index.md
