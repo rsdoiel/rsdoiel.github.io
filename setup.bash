34745149dde8f9f7f60fd9cf5bfd50e54c4048c3
R. S. Doiel <rsdoiel@gmail.com>
2017-01-21 12:37:45 -0800

    save and publish

--- setup.bash
+++ setup.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 function installWebTools() {
-    for GOPROGS in $@; do
+    for GOPROG in "$@"; do
         if [ ! -f $HOME/bin/$GOPROG ]; then
+            echo "Installing $GOPROG"
             go get github.com/rsdoiel/$GOPROG/...
         fi
     done
 }
 
 if [ ! -f $HOME/bin/mergepath ]; then
+    echo "Missing shell tools, installing"
     go get github.com/rsdoiel/shelltools/...
 fi
 installWebTools mkpage mkslides
