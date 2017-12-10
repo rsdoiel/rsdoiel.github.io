4398f4c26876a4dce8e07974d8c9e2ea29d37a01
R. S. Doiel <rsdoiel@gmail.com>
2017-12-10 09:07:46 -0800

    Quick Save

--- setup.bash
+++ setup.bash
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-function installWebTools() {
-    for GOPROG in "$@"; do
-        if [ ! -f $HOME/bin/$GOPROG ]; then
-            echo "Installing $GOPROG"
-            go get -u github.com/caltechlibrary/$GOPROG/...
-        fi
-    done
+function install_tools() {
+	START=$(pwd)
+	for GO_PKG in "$@"; do
+		if [ ! -d $HOME/src/$GO_PKG ]; then
+			echo "Installing $GO_PKG"
+			go get -u $GO_PKG/...
+		else
+			cd "$HOME/src/$GO_PKG"
+			if [ -f Makefile ]; then
+				make install
+			else
+				echo "Skipping $GO_PKG"
+			fi
+			cd "$START"
+		fi
+	done
 }
 
-if [ ! -f $HOME/bin/mergepath ]; then
-    echo "Missing datatools, installing"
-    go get -u github.com/caltechlibrary/datatools/...
-fi
-installWebTools mkpage
+install_tools \
+	mvdan.cc/sh/cmd/shfmt \
+	github.com/caltechlibrary/datatools \
+	github.com/caltechlibrary/mkpage
