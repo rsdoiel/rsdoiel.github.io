8ed081b65acbb73946249c2209038e38766d6672
R. S. Doiel <rsdoiel@gmail.com>
2022-11-17 15:35:18 -0800

    Quick Save

--- mk-gemini-pages.bash
+++ mk-gemini-pages.bash
@@ -2,7 +2,8 @@
 
 find . -type f | grep -E '\.md$' | while read -r FNAME; do
     printf "Coverting %s\n" "${FNAME}"
-    md2gemini -m -w -f -i tab --plain -l paragraph \
+	DNAME=$(dirname "${FNAME}")
+    md2gemini -m -w -d "${DNAME}" -f -i tab --plain -l paragraph \
         -b gemini://sdf.org/rsdoiel/ \
         "${FNAME}"
 done
